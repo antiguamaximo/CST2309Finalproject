18c805e5d2ae37787f02215d26a8748e10672e8a
mantigua911 <81933239+antiguamaximo@users.noreply.github.com>
2024-12-05 14:12:59 -0500

    Fixes
    - Fixed issue related to the validation of input

--- Javascript/validationForm.js
+++ Javascript/validationForm.js
@@ -7,78 +7,67 @@
 */
 let regButton = document.getElementById("regButton");
 
-regButton.addEventListener("click", validatePassword());
+regButton.addEventListener("click", validatePassword);
 
-// regButton.addEventListener("click", validateFName());
+regButton.addEventListener("click", validateName);
 
-// regButton.addEventListener("click", validateEmail());
+regButton.addEventListener("click", validateEmail);
 
 function validatePassword() {
   let password = document.getElementById("password");
   let cPassword = document.getElementById("cPassword");
-
-  var hasUpperCase = /[A-Z]/.test(password);
-  var hasLowerCase = /[a-z]/.test(password);
-  var hasDigits = /\d/.test(password);
-  var hasSpecialChars = /[!@#$%^&*(),.?":{}|<>]/.test(password);
-
+  
   if (password.validity.valueMissing){
     password.setCustomValidity("Enter a password.");
-  } else if (hasLowerCase) {
+  } else if (!(/[a-z]/.test(password.value))) {
     password.setCustomValidity("Password must contain a lower case letter.");
-  } else if (hasUpperCase) {
+  } else if (!(/[A-Z]/.test(password.value))) {
     password.setCustomValidity("Password must contain a upper case letter.");
-  } else if (hasSpecialChars){
+  } else if (!(/[!@#$%^&*(),.?":{}|<>]/.test(password.value))){
     password.setCustomValidity("Password must contain special characters");
-  } else if (hasDigits) {
+  } else if (!(/\d/.test(password.value))) {
     password.setCustomValidity("Password must contain a digits.");
   } else if (password.validity.tooShort) {
     password.setCustomValidity("Password must contain at least 6 characters.");
+  } else if (password.value !== cPassword.value) {
+    password.setCustomValidity("Passwords do not match!");
   } else {
     password.setCustomValidity("");
   }
 
-  if (password != cPassword) {
-    cPassword.setCustomValidity("Passwords do not match!");
-  }
 }
 
-// function validateName() {
-//   var fname = document.getElementById('fname').value;
-//   var fnameRegex = /^[a-zA-Z\s]+$/; 
+function validateName() {
+  let fname = document.getElementById("fName");
 
-//   if (fname === "") {
-//       alert("Name cannot be empty.");
-//       return false;
-//   } else if (!fnameRegex.test(fname)) {
-//       alert("Name can only contain letters and spaces.");
-//       return false;
-//   } else {return true;}
+  if (fname.validity.valueMissing){
+    fname.setCustomValidity("Enter your name.");
+  } else if (!(/^[a-zA-Z\s]+$/.test(fname.value))) {
+    fname.setCustomValidity("Name can only contain letters and spaces.");
+  } else if (fname.validity.tooShort) {
+    fname.setCustomValidity("Name must contain at least 3 characters.");
+  } else {
+    fname.setCustomValidity("");
+  }
   
-// }
+}
 
-// function validateEmail() {
+function validateEmail() {
+  let email = document.getElementById('email');
+  let emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 
-//   var email = document.getElementById('email').value;
-//   var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-
-//   if (!emailRegex.test(email)) {
-//       alert("Please enter a valid email address.");
-//       return false;
-
-//   } else {
-//       return true;
-//   }
+  if (!emailRegex.test(email.value)) {
+      email.setCustomValidity("Please enter a valid email address");
+  }else {
+    email.setCustomValidity("");
+  }
   
-// }
+}
 
 
-// function validateRegForm() {
-//   if (!validatePassword() || validateName() ||  validateEmail()) {
-//       return false;
-//   }else {
-//     alert("Registration complete!");
-//     return true;
-//   }
+window.addEventListener("load", function() {
+  let regForm = document.forms.regForm;
+  let model = regForm.elements.model;
 
-// }
+  model.focus()
+});
